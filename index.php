2f55dba94c668b6f4e11ab69f8ea46b3c81b72df
Natan Ribeiro <sundaycrazygameovertube@gmail.com>
2025-03-03 10:20:06 -0300

    refactor: separate autoload responsibility into Config/Autoloader for better modularization

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
 <?php 
 
+require_once __DIR__ . '/src/Config/Autoloader.php';
+
 use Core\Core; 
+use Config\Autoloader;
 use Router\Routes; 
 
-//Autoload
-spl_autoload_register(function ($class) {
-    
-    $file = __DIR__ . "/src/" . str_replace('\\', '/', $class) . ".php";
-
-    
-    if (file_exists($file)) {
-        require_once $file;
-    }
-});
-
+Autoloader::register();
 
 $routes = Routes::getRoutes(); 
 
